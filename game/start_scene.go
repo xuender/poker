095eb941dbecb5baec491d38e625403441e494bc
miaoyin <miaoyin@woda.com>
2023-07-10 16:24:18 +0800

    启动输入界面

--- game/start_scene.go
+++ game/start_scene.go
@@ -3,9 +3,13 @@ package game
 import (
 	"image/color"
 
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/samber/lo"
+	"github.com/xuender/kit/logs"
 	"github.com/xuender/poker/fonts"
 	"golang.org/x/image/font"
 )
@@ -18,18 +22,151 @@ const (
 type StartScene struct {
 	bus  *Bus
 	face font.Face
+	ui   *ebitenui.UI
 }
 
+// nolint: gomnd
 func NewStart(bus *Bus) *StartScene {
-	start := &StartScene{bus: bus, face: fonts.Head(_fontSize)}
+	start := &StartScene{bus: bus, face: fonts.Body(_fontSize)}
+	buttonImage, _ := loadButtonImage()
+	rootContainer := widget.NewContainer(
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{0x13, 0x1a, 0x22, 0xff})),
+		widget.ContainerOpts.Layout(
+			widget.NewRowLayout(
+				widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+				widget.RowLayoutOpts.Spacing(35),
+				widget.RowLayoutOpts.Padding(widget.NewInsetsSimple(30)),
+			),
+		),
+	)
+
+	button1 := start.newButton("主机", buttonImage, nil)
+	rootContainer.AddChild(button1)
+
+	button2 := start.newButton("加入", buttonImage, nil)
+	rootContainer.AddChild(button2)
+	// construct a standard textinput widget
+	standardTextInput := start.newInput()
+	rootContainer.AddChild(standardTextInput)
+	widget.NewRadioGroup(
+		widget.RadioGroupOpts.Elements(button1, button2),
+		widget.RadioGroupOpts.ChangedHandler(func(args *widget.RadioGroupChangedEventArgs) {
+			// fmt.Println(args.Active.(*widget.Button).Text().Label)
+			if args.Active == button1 {
+				standardTextInput.GetWidget().Disabled = true
+			} else {
+				standardTextInput.GetWidget().Disabled = false
+				standardTextInput.Focus(true)
+			}
+		}),
+	)
+
+	button := start.newButton("确定", buttonImage, func(args *widget.ButtonClickedEventArgs) {
+		logs.D.Println("button clicked")
+	})
+	rootContainer.AddChild(button)
+
+	ui := &ebitenui.UI{
+		Container: rootContainer,
+	}
+
+	start.ui = ui
 
 	return start
 }
 
-func (p *StartScene) Update() error               { return nil }
+// nolint: gomnd
+func (p *StartScene) newInput() *widget.TextInput {
+	input := widget.NewTextInput(
+		widget.TextInputOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionCenter,
+				Stretch:  true,
+			}),
+		),
+
+		widget.TextInputOpts.Image(&widget.TextInputImage{
+			Idle:     image.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 100, A: 255}),
+			Disabled: image.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 100, A: 255}),
+		}),
+
+		widget.TextInputOpts.Face(p.face),
+
+		widget.TextInputOpts.Color(&widget.TextInputColor{
+			Idle:          color.NRGBA{254, 255, 255, 255},
+			Disabled:      color.NRGBA{R: 200, G: 200, B: 200, A: 255},
+			Caret:         color.NRGBA{254, 255, 255, 255},
+			DisabledCaret: color.NRGBA{R: 200, G: 200, B: 200, A: 255},
+		}),
+
+		widget.TextInputOpts.Padding(widget.NewInsetsSimple(5)),
+
+		widget.TextInputOpts.CaretOpts(
+			widget.CaretOpts.Size(p.face, 2),
+		),
+
+		widget.TextInputOpts.Placeholder("192.168.1.234:3880"),
+
+		widget.TextInputOpts.SubmitHandler(func(args *widget.TextInputChangedEventArgs) {
+			logs.D.Println("Text Submitted: ", args.InputText)
+		}),
+
+		widget.TextInputOpts.ChangedHandler(func(args *widget.TextInputChangedEventArgs) {
+			logs.D.Println("Text Changed: ", args.InputText)
+		}),
+	)
+
+	input.InputText = "192.168.1.234:3880"
+
+	return input
+}
+
+// nolint: gomnd
+func (p *StartScene) newButton(
+	label string,
+	buttonImage *widget.ButtonImage,
+	handler widget.ButtonClickedHandlerFunc,
+) *widget.Button {
+	opts := []widget.ButtonOpt{
+		widget.ButtonOpts.WidgetOpts(
+
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionCenter,
+			}),
+		),
+
+		widget.ButtonOpts.Image(buttonImage),
+
+		widget.ButtonOpts.Text(label, p.face, &widget.ButtonTextColor{
+			Idle: color.NRGBA{0xdf, 0xf4, 0xff, 0xff},
+		}),
+
+		widget.ButtonOpts.TextPadding(widget.Insets{
+			Left:   30,
+			Right:  30,
+			Top:    5,
+			Bottom: 5,
+		}),
+	}
+
+	if handler != nil {
+		opts = append(opts, widget.ButtonOpts.ClickedHandler(handler))
+	}
+
+	return widget.NewButton(opts...)
+}
+
+func (p *StartScene) Update() error {
+	p.ui.Update()
+
+	return nil
+}
+
 func (p *StartScene) Keys() map[ebiten.Key]func() { return nil }
 
 func (p *StartScene) Draw(screen *ebiten.Image) {
+	p.ui.Draw(screen)
+
 	max := lo.MaxBy(p.bus.Start, func(a, b string) bool { return len(a) > len(b) })
 	width, height := p.bus.Layout()
 	left := (width - len(max)*_fontSize/_two) / _two
@@ -38,4 +175,19 @@ func (p *StartScene) Draw(screen *ebiten.Image) {
 	for index, txt := range p.bus.Start {
 		text.Draw(screen, txt, p.face, left, top+index*_fontSize, color.RGBA{0xdf, 0xd0, 0x00, 0xff})
 	}
+}
+
+// nolint: gomnd
+func loadButtonImage() (*widget.ButtonImage, error) {
+	idle := image.NewNineSliceColor(color.NRGBA{R: 170, G: 170, B: 180, A: 255})
+
+	hover := image.NewNineSliceColor(color.NRGBA{R: 130, G: 130, B: 150, A: 255})
+
+	pressed := image.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 120, A: 255})
+
+	return &widget.ButtonImage{
+		Idle:    idle,
+		Hover:   hover,
+		Pressed: pressed,
+	}, nil
 }
--- go.mod
+++ go.mod
@@ -5,18 +5,20 @@ go 1.18
 // replace github.com/xx/xx => ../xx
 
 require (
+	github.com/ebitenui/ebitenui v0.5.4
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/google/wire v0.5.0
 	github.com/hajimehoshi/ebiten/v2 v2.5.5
 	github.com/samber/lo v1.38.1
 	github.com/xtaci/kcp-go/v5 v5.6.2
 	github.com/xuender/kit v1.0.17
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
-	golang.org/x/image v0.6.0
+	golang.org/x/image v0.7.0
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
-	github.com/ebitengine/purego v0.3.0 // indirect
+	github.com/ebitengine/purego v0.3.2 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.14 // indirect
@@ -27,9 +29,9 @@ require (
 	github.com/tjfoc/gmsm v1.4.1 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/exp/shiny v0.0.0-20230626212559-97b1e661b5df // indirect
-	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
+	golang.org/x/mobile v0.0.0-20230427221453-e8d11dd0ba41 // indirect
 	golang.org/x/net v0.6.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
